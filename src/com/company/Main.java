2d0220e5fb58476a717d9c2a45929e2f0068f4a9
Viktoriia20 <[nikas.viks@gmail.com]>
2020-07-11 18:13:15 +0300

    Text updated to Viktorii2022

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Viktoriia20");
+        System.out.println("Viktoriia2022");
     }
 }
